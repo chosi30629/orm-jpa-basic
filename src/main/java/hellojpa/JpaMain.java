8aeedbbb5a86db2386419798d139b4e1d8408b4d
조성일 <chosi30629@gmail.com>
2019-11-09 22:51:42 +0900

    영속성 컨텍스트 1

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -18,39 +18,16 @@ public class JpaMain {
 
         try {
 
-            /*
-            // 등록
+            // 비영속
             Member member = new Member();
-            member.setId(1L);
-            member.setName("HelloA");
+            member.setId(100L);
+            member.setName("HelloJPA");
+
+            // 영속
+            System.out.println("=== BEFORE ===");
             em.persist(member);
-            */
-
-            /*
-            // 수정
-            Member findMember = em.find(Member.class, 1L);
-            findMember.setName("HelloJPA");
-            */
-
-            /*
-            // 삭제
-            Member findMember = em.find(Member.class, 1L);
-            em.remove(findMember);
-            */
-
-            /*
-            // PK 로 조회
-            Member findMember = em.find(Member.class, 1L);
-            */
-
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)
-                    .setMaxResults(8)
-                    .getResultList();
-
-            for (Member member : result) {
-                System.out.println("member.name = " + member.getName());
-            }
+//            em.detach(member); 준영속
+            System.out.println("=== AFTER ===");
 
             tx.commit();
         } catch (Exception e) {

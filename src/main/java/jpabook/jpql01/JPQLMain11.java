a6e46c3044b3c42b7b16c333347757a34eee3f31
조성일 <chosi30629@gmail.com>
2019-11-24 21:24:35 +0900

    객체지향 쿼리 언어1 - JPQL 함수

---
+++ src/main/java/jpabook/jpql01/JPQLMain11.java
@@ -0,0 +1,69 @@
+package jpabook.jpql01;
+
+import jpabook.jpql01.domain.JPQLMember;
+import jpabook.jpql01.domain.JPQLTeam;
+import jpabook.jpql01.domain.MemberType;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JPQLMain11 {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            JPQLTeam team = new JPQLTeam();
+            team.setName("관리자");
+            em.persist(team);
+
+            JPQLMember member = new JPQLMember();
+            member.setUsername("choseongil");
+            member.setAge(10);
+            member.changeTeam(team);
+            member.setType(MemberType.ADMIN);
+
+            JPQLMember member1 = new JPQLMember();
+            member1.setUsername("kkk");
+            member1.setAge(10);
+            member1.changeTeam(team);
+            member1.setType(MemberType.ADMIN);
+
+            em.persist(member);
+            em.persist(member1);
+
+            em.flush();
+            em.clear();
+
+            String query2 =
+//                    "select 'a' || 'b' from JPQLMember m";
+//                    "select concat('a', 'b') from JPQLMember m";
+//                    "select substring(m.username, 2, 3) from JPQLMember m";
+//                    "select locate('de', 'abcdefg') from JPQLMember m"; // Integer 타입
+//                    "select size(t.members) from JPQLTeam t";
+//                    "select index(t.members) from JPQLTeam t";
+//                    "select function('group_concat', m.username) from JPQLMember m";
+                    "select group_concat(m.username) from JPQLMember m";    // hibernate 사용 시
+
+            List<String> result2 = em.createQuery(query2, String.class)
+                    .getResultList();
+
+            for (String s : result2) {
+                System.out.println("s = " + s);
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+
+}
